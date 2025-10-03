2e21a1f876cedb81689e360cb65d83ff1fca2bd5
ayim-en <ayimen.dev@gmail.com>
2025-10-02 21:11:22 -0700

    installing main dependencies

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,15 @@
+import { PortalHost } from "@rn-primitives/portal";
 import { Stack } from "expo-router";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import "../global.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <>
+    <GestureHandlerRootView>
+      <Stack screenOptions={{headerShown: false}} />;
+      <PortalHost />
+    </GestureHandlerRootView>
+    </>
+  );
 }
