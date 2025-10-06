9712f1a8e9a1295b5bdfcc21f3d600e2325a6851
ayim-en <ayimen.dev@gmail.com>
2025-10-05 21:40:05 -0700

    router fix

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,27 +6,34 @@ import "../global.css";
 export default function RootLayout() {
   return (
     <>
-      <GestureHandlerRootView style = {{ flex: 1}}>
-      <Tabs screenOptions={{headerShown: false}}>
-        <Tabs.Screen name="index"
-        options={{
-            title: "Home"
-        }}/>
-        <Tabs.Screen name="qibla"
-          options={{
-            title: "Qibla"
-        }}/>
-        <Tabs.Screen name="calendar"
-          options={{
-            title: "Calendar"
-        }}/>
-        <Tabs.Screen name="settings_options"
-        options={{
-            title: "Settings",
-            href: null
-        }}/>
-      </Tabs>
-      <PortalHost />
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <Tabs screenOptions={{ headerShown: false }}>
+          <Tabs.Screen
+            name="index"
+            options={{
+              title: "Home",
+            }}
+          />
+          <Tabs.Screen
+            name="qibla"
+            options={{
+              title: "Qibla",
+            }}
+          />
+          <Tabs.Screen
+            name="calendar"
+            options={{
+              title: "Calendar",
+            }}
+          />
+          <Tabs.Screen
+            name="settings_options"
+            options={{
+              title: "Settings",
+            }}
+          />
+        </Tabs>
+        <PortalHost />
       </GestureHandlerRootView>
     </>
   );
